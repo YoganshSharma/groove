968da8d0b22377c3c43059686aaf54129f959d11
Yogansh Sharma <yogansh@yogansh.tech>
2026-06-21 00:17:33 +0530

    fix:activity watch

--- home/services/system/activitywatch.nix
+++ home/services/system/activitywatch.nix
@@ -10,7 +10,7 @@
           poll_time = 2;
         };
       };
-      aw-watcher-window = {
+      aw-watcher-window-wayland = {
         package = pkgs.aw-watcher-window-wayland;
         settings = {
           poll_time = 1;
